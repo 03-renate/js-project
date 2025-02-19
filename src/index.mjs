021e877abb35b6b3c1dadad4bcec07fa8cd0669a
03-renate <renate.pedersen03@gmail.com>
2025-02-19 16:16:15 +0100

    feat/review+commenting

--- src/index.mjs
+++ src/index.mjs
@@ -1,33 +1,37 @@
+//GLOBAL VARIABLES
 const API_URL = "https://v2.api.noroff.dev/rainy-days";
 const containerElement = document.querySelector("#js-products")
 const CURRENCY = "NOK"
 
+
+//CHECK IF containerElement EXIST IN THE DOM
 if (!containerElement) { //if these elements do not exist in the DOM, it will bum out
     console.error("JS cannot run");
 } else {
-    setup();
+    setup(); //if element exist, call SETUP to proceed with initialization 
 }
 
+//FUNCTION - INITIALIZE THE APPLICATION SETUP
 function setup(){
-    getProducts(); //calling GET PRODUCTS function
+    getProducts(); //calling GET PRODUCTS
 }
 
-//GET PRODUCTS
+
+//FUNCTION - FETCHES AND DISPLAYS PRODUCTS
 async function getProducts() {
     try{
         const response = await fetch(API_URL);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const {data} = await response.json();
         
-
+        //loop through each item in the array
         data.forEach(item => {
-            const template = itemTemplate({
+            const template = itemTemplate({ //generate a string template
             title: item.title,
-            imageURL: item.image?.url, //? > object is null > expression evaluates to undefined instead of error
-            imageAlt: item.image?.alt, //? > object is not null -> proceeds to access the url property
+            imageURL: item.image?.url, //? = safe access > object is null -> expression evaluates to undefined instead of throwing error
+            imageAlt: item.image?.alt, //? = safe access > object is not null -> proceeds to access the url property
             description: item.description, 
             price: item.price, 
             id: item.id,
@@ -64,7 +68,7 @@ function itemTemplate({
 }
 
 
-//FUNCTION - 
+//FUNCTION - CREATE HTML ELEMENT FROM A STRING TEMPLATE
 function createHTML(template){
     const parser = new DOMParser();
     const parsedDocument = parser.parseFromString(template, "text/html");
